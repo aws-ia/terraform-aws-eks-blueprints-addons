7427f33a35117f0f1a3bdb0bb980fb621a6a6408
Tony Bulding <44652148+tbulding@users.noreply.github.com>
2023-02-08 14:21:01 -0600

    Initial commit

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 0.24.0"
+    }
+  }
+}
