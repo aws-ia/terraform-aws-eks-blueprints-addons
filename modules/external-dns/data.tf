4fb3165b25844ea3e6c5b925ec557d17cd0fa0e0
Carlos Santana <csantana23@gmail.com>
2023-03-17 15:19:53 -0400

    remove deprecated options for external-dns module
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- modules/external-dns/data.tf
+++ modules/external-dns/data.tf
@@ -1,10 +1,7 @@
 data "aws_iam_policy_document" "external_dns_iam_policy_document" {
   statement {
     effect = "Allow"
-    resources = distinct(concat(
-      [data.aws_route53_zone.selected.arn],
-      var.route53_zone_arns
-    ))
+    resources = distinct(var.route53_zone_arns)
     actions = [
       "route53:ChangeResourceRecordSets",
       "route53:ListResourceRecordSets",
