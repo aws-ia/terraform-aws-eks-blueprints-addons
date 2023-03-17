fec9c318b181e987933cc0bbe854005a23e36a95
Carlos Santana <csantana23@gmail.com>
2023-03-17 15:46:08 -0400

    ran pre-commit
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- modules/external-dns/data.tf
+++ modules/external-dns/data.tf
@@ -1,6 +1,6 @@
 data "aws_iam_policy_document" "external_dns_iam_policy_document" {
   statement {
-    effect = "Allow"
+    effect    = "Allow"
     resources = distinct(var.route53_zone_arns)
     actions = [
       "route53:ChangeResourceRecordSets",
