6b86514a6570c62ba450d0373acbdb688c8e9ada
Bryant Biggs <bryantbiggs@gmail.com>
2023-04-06 19:42:31 -0400

    refactor: Update `cluster-autoscaler` to use new addon module (#100)

--- main.tf
+++ main.tf
@@ -919,6 +919,161 @@ module "aws_load_balancer_controller" {
   tags = var.tags
 }
 
+################################################################################
+# Cluster Autoscaler
+################################################################################
+
+locals {
+  cluster_autoscaler_service_account = try(var.cluster_autoscaler.service_account_name, "external-secrets-sa")
+
+  # Lookup map to pull latest cluster-autoscaler patch version given the cluster version
+  cluster_autoscaler_image_tag = {
+    "1.20" = "v1.20.3"
+    "1.21" = "v1.21.3"
+    "1.22" = "v1.22.3"
+    "1.23" = "v1.23.1"
+    "1.24" = "v1.24.1"
+    "1.25" = "v1.25.1"
+    "1.26" = "v1.26.2"
+  }
+}
+
+# https://github.com/external-secrets/kubernetes-external-secrets#add-a-secret
+data "aws_iam_policy_document" "cluster_autoscaler" {
+  count = var.enable_cluster_autoscaler ? 1 : 0
+
+  statement {
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeScalingActivities",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeInstanceTypes",
+      "eks:DescribeNodegroup",
+      "ec2:DescribeImages",
+      "ec2:GetInstanceTypesFromInstanceRequirements"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.cluster_name}"
+      values   = ["owned"]
+    }
+  }
+}
+
+module "cluster_autoscaler" {
+  # source = "aws-ia/eks-blueprints-addon/aws"
+  source = "./modules/eks-blueprints-addon"
+
+  create = var.enable_cluster_autoscaler
+
+  # https://github.com/external-secrets/external-secrets/blob/main/deploy/charts/external-secrets/Chart.yaml
+  name             = try(var.cluster_autoscaler.name, "cluster-autoscaler")
+  description      = try(var.cluster_autoscaler.description, "A Helm chart to deploy cluster-autoscaler")
+  namespace        = try(var.cluster_autoscaler.namespace, "kube-system")
+  create_namespace = try(var.cluster_autoscaler.create_namespace, false)
+  chart            = "cluster-autoscaler"
+  chart_version    = try(var.cluster_autoscaler.chart_version, "9.28.0")
+  repository       = try(var.cluster_autoscaler.repository, "https://kubernetes.github.io/autoscaler")
+  values           = try(var.cluster_autoscaler.values, [])
+
+  timeout                    = try(var.cluster_autoscaler.timeout, null)
+  repository_key_file        = try(var.cluster_autoscaler.repository_key_file, null)
+  repository_cert_file       = try(var.cluster_autoscaler.repository_cert_file, null)
+  repository_ca_file         = try(var.cluster_autoscaler.repository_ca_file, null)
+  repository_username        = try(var.cluster_autoscaler.repository_username, null)
+  repository_password        = try(var.cluster_autoscaler.repository_password, null)
+  devel                      = try(var.cluster_autoscaler.devel, null)
+  verify                     = try(var.cluster_autoscaler.verify, null)
+  keyring                    = try(var.cluster_autoscaler.keyring, null)
+  disable_webhooks           = try(var.cluster_autoscaler.disable_webhooks, null)
+  reuse_values               = try(var.cluster_autoscaler.reuse_values, null)
+  reset_values               = try(var.cluster_autoscaler.reset_values, null)
+  force_update               = try(var.cluster_autoscaler.force_update, null)
+  recreate_pods              = try(var.cluster_autoscaler.recreate_pods, null)
+  cleanup_on_fail            = try(var.cluster_autoscaler.cleanup_on_fail, null)
+  max_history                = try(var.cluster_autoscaler.max_history, null)
+  atomic                     = try(var.cluster_autoscaler.atomic, null)
+  skip_crds                  = try(var.cluster_autoscaler.skip_crds, null)
+  render_subchart_notes      = try(var.cluster_autoscaler.render_subchart_notes, null)
+  disable_openapi_validation = try(var.cluster_autoscaler.disable_openapi_validation, null)
+  wait                       = try(var.cluster_autoscaler.wait, null)
+  wait_for_jobs              = try(var.cluster_autoscaler.wait_for_jobs, null)
+  dependency_update          = try(var.cluster_autoscaler.dependency_update, null)
+  replace                    = try(var.cluster_autoscaler.replace, null)
+  lint                       = try(var.cluster_autoscaler.lint, null)
+
+  postrender = try(var.cluster_autoscaler.postrender, [])
+  set = concat(
+    [
+      {
+        name  = "awsRegion"
+        value = local.region
+      },
+      {
+        name  = "autoDiscovery.clusterName"
+        value = var.cluster_name
+      },
+      {
+        name  = "image.tag"
+        value = try(local.cluster_autoscaler_image_tag[var.cluster_version], "v${var.cluster_version}.0")
+      },
+      {
+        name  = "rbac.serviceAccount.name"
+        value = local.cluster_autoscaler_service_account
+      }
+    ],
+    try(var.cluster_autoscaler.set, [])
+  )
+  set_sensitive = try(var.cluster_autoscaler.set_sensitive, [])
+
+  # IAM role for service account (IRSA)
+  set_irsa_name                 = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+  create_role                   = try(var.cluster_autoscaler.create_role, true)
+  role_name                     = try(var.cluster_autoscaler.role_name, "cluster-autoscaler")
+  role_name_use_prefix          = try(var.cluster_autoscaler.role_name_use_prefix, true)
+  role_path                     = try(var.cluster_autoscaler.role_path, "/")
+  role_permissions_boundary_arn = lookup(var.cluster_autoscaler, "role_permissions_boundary_arn", null)
+  role_description              = try(var.cluster_autoscaler.role_description, "IRSA for cluster-autoscaler operator")
+  role_policies                 = lookup(var.cluster_autoscaler, "role_policies", {})
+
+  source_policy_documents = compact(concat(
+    data.aws_iam_policy_document.cluster_autoscaler[*].json,
+    lookup(var.cluster_autoscaler, "source_policy_documents", [])
+  ))
+  override_policy_documents = lookup(var.cluster_autoscaler, "override_policy_documents", [])
+  policy_statements         = lookup(var.cluster_autoscaler, "policy_statements", [])
+  policy_name               = try(var.cluster_autoscaler.policy_name, null)
+  policy_name_use_prefix    = try(var.cluster_autoscaler.policy_name_use_prefix, true)
+  policy_path               = try(var.cluster_autoscaler.policy_path, null)
+  policy_description        = try(var.cluster_autoscaler.policy_description, "IAM Policy for cluster-autoscaler operator")
+
+  oidc_providers = {
+    this = {
+      provider_arn = var.oidc_provider_arn
+      # namespace is inherited from chart
+      service_account = local.cluster_autoscaler_service_account
+    }
+  }
+
+  tags = var.tags
+}
+
 #-----------------Kubernetes Add-ons----------------------
 
 module "argocd" {
@@ -975,17 +1130,6 @@ module "cert_manager" {
   install_letsencrypt_issuers       = var.cert_manager_install_letsencrypt_issuers
   letsencrypt_email                 = var.cert_manager_letsencrypt_email
   kubernetes_svc_image_pull_secrets = var.cert_manager_kubernetes_svc_image_pull_secrets
-}
-
-module "cluster_autoscaler" {
-  source = "./modules/cluster-autoscaler"
-
-  count = var.enable_cluster_autoscaler ? 1 : 0
-
-  eks_cluster_version = var.cluster_version
-  helm_config         = var.cluster_autoscaler_helm_config
-  manage_via_gitops   = var.argocd_manage_add_ons
-  addon_context       = local.addon_context
 }
 
 module "external_dns" {
