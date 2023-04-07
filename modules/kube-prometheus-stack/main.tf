739dfd5eaebad57f1393983276ae217a9f0e9ea6
Parker Barthlome <pbbarthlome@msn.com>
2023-04-07 16:32:54 -0600

    fix: Remove namespace creation for kube-prometheus-stack (#103)

--- modules/kube-prometheus-stack/main.tf
+++ modules/kube-prometheus-stack/main.tf
@@ -7,23 +7,18 @@ locals {
   }
 }
 
-resource "kubernetes_namespace_v1" "prometheus" {
-  metadata {
-    name = local.namespace
-  }
-}
-
 # https://github.com/prometheus-community/helm-charts/blob/main/charts/kube-prometheus-stack/Chart.yaml
 module "helm_addon" {
   source = "../helm-addon"
 
   helm_config = merge(
     {
-      name       = local.name
-      chart      = local.name
-      repository = "https://prometheus-community.github.io/helm-charts"
-      version    = "45.7.1"
-      namespace  = kubernetes_namespace_v1.prometheus.metadata[0].name
+      name             = local.name
+      chart            = local.name
+      repository       = "https://prometheus-community.github.io/helm-charts"
+      version          = "45.7.1"
+      namespace        = local.namespace
+      create_namespace = local.namespace != "kube-system" ? try(var.helm_config.create_namespace, true) : false
       values = [templatefile("${path.module}/values.yaml", {
         aws_region = var.addon_context.aws_region_name
       })]
