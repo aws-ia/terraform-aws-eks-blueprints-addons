6bec675f30ac6f9c338f0c5a810616c056b16885
Apoorva Kulkarni <kuapoorv@amazon.com>
2023-04-17 06:45:28 -0700

    refactor: Update ingress-nginx addon to use new addon module

--- outputs.tf
+++ outputs.tf
@@ -83,6 +83,11 @@ output "external_dns" {
   value       = module.external_dns
 }
 
+output "ingress_nginx" {
+  description = "Map of attributes of the Helm release and IRSA created"
+  value       = module.ingress_nginx
+}
+
 output "fargate_fluentbit" {
   description = "Map of attributes of the Helm release and IRSA created"
   value       = try(module.fargate_fluentbit[0], null)
@@ -96,11 +101,6 @@ output "gatekeeper" {
 output "grafana" {
   description = "Map of attributes of the Helm release and IRSA created"
   value       = try(module.grafana[0], null)
-}
-
-output "ingress_nginx" {
-  description = "Map of attributes of the Helm release and IRSA created"
-  value       = try(module.ingress_nginx[0], null)
 }
 
 output "karpenter" {
