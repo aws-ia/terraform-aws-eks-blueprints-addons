135252ee73e0d864e480578471f9a624c4e9b510
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-04-18 09:39:00 -0400

    fix: Fix InstanceProfile references in `locals` and `aws_iam_instance_profile.karpenter` resource (#131)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- main.tf
+++ main.tf
@@ -1743,7 +1743,7 @@ locals {
   karpenter_service_account_name    = try(var.karpenter.service_account_name, "karpenter")
   karpenter_enable_spot_termination = var.enable_karpenter && var.karpenter_enable_spot_termination
   create_karpenter_instance_profile = try(var.karpenter_instance_profile.create, true)
-  karpenter_instance_profile_name   = try(aws_iam_instance_profile.karpenter[0].arn, var.karpenter_instance_profile.name, "")
+  karpenter_instance_profile_name   = try(aws_iam_instance_profile.karpenter[0].name, var.karpenter_instance_profile.name, "")
 }
 
 data "aws_iam_role" "karpenter" {
