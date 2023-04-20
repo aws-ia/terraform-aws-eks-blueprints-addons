07fd6dc56e7cccd267087e758dcbd34e42db0159
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-04-20 19:18:38 -0400

    Fixing `gatekeeper` Namespace

--- main.tf
+++ main.tf
@@ -2557,7 +2557,7 @@ module "gatekeeper" {
   name             = try(var.gatekeeper.name, local.cluster_proportional_autoscaler_name)
   description      = try(var.gatekeeper.description, "A Helm chart to install Gatekeeper")
   namespace        = try(var.gatekeeper.namespace, "gatekeeper-system")
-  create_namespace = try(var.gatekeeper.create_namespace, false)
+  create_namespace = try(var.gatekeeper.create_namespace, true)
   chart            = local.gatekeeper_name
   chart_version    = try(var.gatekeeper.chart_version, "3.12.0")
   repository       = try(var.gatekeeper.repository, "https://open-policy-agent.github.io/gatekeeper/charts")
