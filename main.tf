934a1eab9d2a9de68d1094a634cdea0ddad48f9d
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-04-27 19:51:05 -0400

    fix: Velero `serviceAccount` IRSA configuration and `resouces` Policy (#147)

--- main.tf
+++ main.tf
@@ -2742,10 +2742,11 @@ module "secrets_store_csi_driver_provider_aws" {
 ################################################################################
 
 locals {
-  velero_service_account         = try(var.velero.service_account_name, "velero-sa")
+  velero_name                    = "velero"
+  velero_service_account         = try(var.velero.service_account_name, "${local.velero_name}-server")
   velero_backup_s3_bucket        = split(":", var.velero.s3_backup_location)
   velero_backup_s3_bucket_arn    = try(split("/", var.velero.s3_backup_location)[0], var.velero.s3_backup_location)
-  velero_backup_s3_bucket_name   = try(split("/", local.velero_backup_s3_bucket[5])[1], local.velero_backup_s3_bucket[5])
+  velero_backup_s3_bucket_name   = try(split("/", local.velero_backup_s3_bucket[5])[0], local.velero_backup_s3_bucket[5])
   velero_backup_s3_bucket_prefix = try(split("/", var.velero.s3_backup_location)[1], "")
 }
 
@@ -2784,7 +2785,7 @@ data "aws_iam_policy_document" "velero" {
       "s3:ListMultipartUploadParts",
       "s3:PutObject",
     ]
-    resources = [local.velero_backup_s3_bucket_prefix == "" ? "${var.velero.s3_backup_location}/*" : var.velero.s3_backup_location]
+    resources = ["${var.velero.s3_backup_location}/*"]
   }
 
   statement {
@@ -2849,7 +2850,7 @@ module "velero" {
             EOT
     },
     {
-      name  = "serviceAccount.name"
+      name  = "serviceAccount.server.name"
       value = local.velero_service_account
     },
     {
@@ -2865,6 +2866,10 @@ module "velero" {
       value = local.velero_backup_s3_bucket_name
     },
     {
+      name  = "configuration.backupStorageLocation.config.region"
+      value = local.region
+    },
+    {
       name  = "configuration.volumeSnapshotLocation.config.region"
       value = local.region
     },
@@ -2877,7 +2882,7 @@ module "velero" {
   set_sensitive = try(var.velero.set_sensitive, [])
 
   # IAM role for service account (IRSA)
-  set_irsa_names                = ["serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"]
+  set_irsa_names                = ["serviceAccount.server.annotations.eks\\.amazonaws\\.com/role-arn"]
   create_role                   = try(var.velero.create_role, true)
   role_name                     = try(var.velero.role_name, "velero")
   role_name_use_prefix          = try(var.velero.role_name_use_prefix, true)
