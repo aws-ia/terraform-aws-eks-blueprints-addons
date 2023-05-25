b625dd0802f295e58a0d14bac6a46418ac0d7d50
Bryant Biggs <bryantbiggs@gmail.com>
2023-05-25 15:48:18 -0400

    chore: Sync changes from Blueprints examples refactoring (ALB controller + Fargate FluentBit) (#163)

--- outputs.tf
+++ outputs.tf
@@ -84,8 +84,11 @@ output "external_secrets" {
 }
 
 output "fargate_fluentbit" {
-  description = "Map of attributes of the Helm release and IRSA created"
-  value       = kubernetes_config_map_v1.aws_logging
+  description = "Map of attributes of the configmap and IAM policy created"
+  value = {
+    configmap  = kubernetes_config_map_v1.aws_logging
+    iam_policy = aws_iam_policy.fargate_fluentbit
+  }
 }
 
 output "gatekeeper" {
