065580849f958024af40c24cf03c84e2a6957543
Bryant Biggs <bryantbiggs@gmail.com>
2023-05-30 15:55:35 -0400

    feat: Update AWS provider min required version to 5.0 to support new EKS addon arguments

--- tests/complete/versions.tf
+++ tests/complete/versions.tf
@@ -6,20 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.47"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.17"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.8"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
   }
-
-  # ##  Used for end-to-end testing on project; update to suit your needs
-  # backend "s3" {
-  #   bucket = "terraform-ssp-github-actions-state"
-  #   region = "us-west-2"
-  #   key    = "e2e/addons/complete/terraform.tfstate"
-  # }
 }
